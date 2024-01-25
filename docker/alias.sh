dddba0fe9812ef4d3e3af644ee3c2acbbbf1a2ad
Robert Butler or someone else at SCCWRP <robertb@sccwrp.org>
2024-01-25 00:14:37 +0000

    shell scripts to speed development

---
+++ docker/alias.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+alias logs='sudo docker container logs -f ai-search-tool';
+alias restart='sudo docker container restart ai-search-tool';
+alias relog='sudo docker container restart ai-search-tool;sudo docker container logs -f ai-search-tool;';
+alias clearlogs='sudo sh -c "echo \"\" > $(docker inspect --format='{{.LogPath}}' ai-search-tool)"';
