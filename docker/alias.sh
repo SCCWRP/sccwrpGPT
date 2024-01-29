9e23041198bb7740c8406bb995e1fbc0cdd22c11
Robert Butler or someone else at SCCWRP <robertb@sccwrp.org>
2024-01-29 21:07:29 +0000

    added dev shell aliases and re organized css and js

--- docker/alias.sh
+++ docker/alias.sh
@@ -4,3 +4,8 @@ alias restart='sudo docker container restart ai-search-tool';
 alias relog='sudo docker container restart ai-search-tool;sudo docker container logs -f ai-search-tool;';
 alias clearlogs='sudo sh -c "echo \"\" > $(docker inspect --format='{{.LogPath}}' ai-search-tool)"';
 alias ipython='sudo docker container exec -it -w /var/www/sccwrpgpt/testing ai-search-tool ipython';
+alias devlogs='sudo docker container logs -f ai-search-tool-dev';
+alias devrestart='sudo docker container restart ai-search-tool-dev';
+alias devrelog='sudo docker container restart ai-search-tool-dev;sudo docker container logs -f ai-search-tool-dev;';
+alias devclearlogs='sudo sh -c "echo \"\" > $(docker inspect --format='{{.LogPath}}' ai-search-tool-dev)"';
+alias devipython='sudo docker container exec -it -w /var/www/sccwrpgpt/testing ai-search-tool-dev ipython';
--- docker/clear_logs.sh
+++ docker/clear_logs.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo echo "" > $(sudo docker inspect --format='{{.LogPath}}' ai-search-tool)
+sudo echo "" > $(sudo docker inspect --format='{{.LogPath}}' ai-search-tool);
+sudo echo "" > $(sudo docker inspect --format='{{.LogPath}}' ai-search-tool-dev);
